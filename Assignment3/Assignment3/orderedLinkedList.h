c398f07c7ef2b2a5c259bd7784b4ab1ba6ec9e3c
Jonathan Mirabile <jonathanmirabile@mail.weber.edu>
2017-02-21 09:48:31 -0700

    Added files to project

---
+++ Assignment3/Assignment3/orderedLinkedList.h
@@ -0,0 +1,92 @@
+#ifndef ORDEREDLINKEDLIST_H
+#define ORDEREDLINKEDLIST_H
+
+template <class Type>
+struct Node
+{
+	Type info;
+	Node *next;
+};
+
+
+template <class Type>
+class OrderedLinkedList
+{
+public:
+	OrderedLinkedList();
+	OrderedLinkedList(const OrderedLinkedList& other);
+	~OrderedLinkedList();
+	
+	OrderedLinkedList<Type>& operator=(const OrderedLinkedList<Type>& other);
+	
+	int insert(const Type&);
+	Type* find(int) const;
+	Type* get(int) const;
+	int remove(int);
+        void clear();
+        int size() const;
+	void print() const;
+};
+
+template <class Type>
+OrderedLinkedList<Type>::OrderedLinkedList()
+{
+}
+
+template <class Type>
+OrderedLinkedList<Type>::OrderedLinkedList(const OrderedLinkedList<Type>& other)
+{
+}
+
+template <class Type>
+OrderedLinkedList<Type>::~OrderedLinkedList()
+{
+}
+
+template <class Type>
+OrderedLinkedList<Type>& OrderedLinkedList<Type>::operator=(const OrderedLinkedList& other)
+{
+return *this;
+}
+
+template <class Type>
+int OrderedLinkedList<Type>::insert(const Type& item)
+{
+return -1;
+}
+
+template <class Type>
+Type* OrderedLinkedList<Type>::get(int dest) const
+{
+return NULL;
+}
+
+template <class Type>
+Type* OrderedLinkedList<Type>::find(int dest) const
+{
+return NULL;
+}
+
+template <class Type>
+int OrderedLinkedList<Type>::remove(int key)
+{
+return 0;
+}
+
+template <class Type>
+void OrderedLinkedList<Type>::clear()
+{
+}
+
+
+template <class Type>
+int OrderedLinkedList<Type>::size() const
+{
+return -1;
+}
+
+template <class Type>
+void OrderedLinkedList<Type>::print() const
+{
+}
+#endif
